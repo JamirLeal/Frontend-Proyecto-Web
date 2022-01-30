0204b0616bce0afb573b449e2aa8016f9567e67b
KevnCor99 <a01733548@tec.mx>
2022-01-29 23:34:19 -0600

    rutas navegacion

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { render } from "react-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+
+render(
+  <BrowserRouter>
+    <Routes>
+           <Route path="/"  element = {<div>Login</div>}/>
+           <Route path="/Calendario" element = {<div>Es es la pagina de Calendario</div>}/>
+           <Route path="/Perfil"  element = {<div>Esta es la pagina de Perfil</div>}/>
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
