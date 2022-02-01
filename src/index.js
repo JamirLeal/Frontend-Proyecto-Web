81f4bbaa0343f447185cdc7937b6f41b205dc0ea
Rodrigo Márquez <a01022943@itesm.mx>
2022-01-31 19:12:51 -0600

    login component

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import { render } from "react-dom";
-import {
-  BrowserRouter,
-  Routes,
-  Route
-} from "react-router-dom";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./components/login/login";
+import "tachyons";
 
 render(
   <BrowserRouter>
     <Routes>
-           <Route path="/"  element = {<div>Login</div>}/>
-           <Route path="/Calendario" element = {<div>Es es la pagina de Calendario</div>}/>
-           <Route path="/Perfil"  element = {<div>Esta es la pagina de Perfil</div>}/>
+      <Route path="/" element={<Login />} />
+      <Route
+        path="/Calendario"
+        element={<div>Es es la pagina de Calendario</div>}
+      />
+      <Route path="/Perfil" element={<div>Esta es la pagina de Perfil</div>} />
     </Routes>
   </BrowserRouter>,
   document.getElementById("root")
