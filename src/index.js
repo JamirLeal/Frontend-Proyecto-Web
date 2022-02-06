cf3821ddef67b11749765807836184cda319c8dd
JamirLeal <a00826275@itesm.mx>
2022-02-05 20:49:05 -0600

    Added calendar.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import { render } from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/login/login";
 import Register from "./components/register/register";
+import MyCalendar from "./components/Calendar/MyCalendar";
+
 import "tachyons";
 
 render(
@@ -12,7 +14,7 @@ render(
       <Route path="/register" element={<Register />} />
       <Route
         path="/Calendario"
-        element={<div>Es es la pagina de Calendario</div>}
+        element={<MyCalendar />}
       />
       <Route path="/Perfil" element={<div>Esta es la pagina de Perfil</div>} />
     </Routes>
